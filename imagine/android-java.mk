7566cdf05878dfdfe6790b0b5dafa416b9724386
Robert Broglia <robert.broglia@gmail.com>
2017-11-12 17:54:14 -0500

    Update build system for Android Studio 3.0
    * Imagine: Update Android Gradle plugin to version 3.0 and wrapper to 4.1
    * Imagine: Remove ANDROID_BUILD_TOOLS_VERSION property, not needed with latest plugin

--- imagine/android-java.mk
+++ imagine/android-java.mk
@@ -3,7 +3,7 @@ include $(IMAGINE_PATH)/make/config.mk
 prefix ?= $(IMAGINE_SDK_PATH)/android-java
 
 imagineV9SrcPath := $(projectPath)/src/base/android/imagine-v9
-imagineV9BuildJarPath := $(imagineV9SrcPath)/build/intermediates/bundles/default/classes.jar
+imagineV9BuildJarPath := $(imagineV9SrcPath)/build/intermediates/bundles/release/classes.jar
 imagineV9InstallJarPath := $(prefix)/imagine-v9.jar
 
 .PHONY: all build clean install install-links
