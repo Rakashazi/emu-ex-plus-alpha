6d0bd5ee3181962ee99ab3bccdf7421059f13a11
soundsnow <109885044+soundsnow@users.noreply.github.com>
2022-07-28 11:35:30 +0800

    md cartridge hardware svp

--- MD.emu/src/genplus-gx/cart_hw/svp/svp.h
+++ MD.emu/src/genplus-gx/cart_hw/svp/svp.h
@@ -17,18 +17,14 @@
 #include "ssp16.h"
 
 typedef struct {
-  unsigned char iram_rom[0x20000]; // IRAM (0-0x7ff) and program ROM (0x800-0x1ffff)
+  unsigned char iram_rom[0x20000]; /* IRAM (0-0x7ff) and program ROM (0x800-0x1ffff) */
   unsigned char dram[0x20000];
   ssp1601_t ssp1601;
 } svp_t;
 
 extern svp_t *svp;
-extern int16 SVP_cycles; 
 
 extern void svp_init(void);
 extern void svp_reset(void);
-extern void svp_write_dram(uint32 address, uint32 data);
-extern uint32 svp_read_cell_1(uint32 address);
-extern uint32 svp_read_cell_2(uint32 address);
 
 #endif
